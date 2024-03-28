d64429aa462c170df97ac7b8db60ae3ff3961978
drshvets <yevhen.shvets@yellows.pl>
2024-03-28 14:18:34 +0100

    add basic vuetity config

---
+++ src/vuetify/vuetity.config.js
@@ -0,0 +1,11 @@
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import { VDataTable } from 'vuetify/components';
+
+const components = {
+  VDataTable
+};
+
+export const vuetify = createVuetify({
+  components
+});
