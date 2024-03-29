ab6e42e456712666665b6c16c6aba9ec96836bbd
drshvets <yevhen.shvets@yellows.pl>
2024-03-29 15:09:34 +0100

    install axios and vue-query, create api client

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import VueDevTools from 'vite-plugin-vue-devtools';
 
-export default defineConfig({
-  plugins: [vue(), VueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [vue(), VueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      proxy: {
+        '/api': process.env.VITE_PLANET_API
+      }
     }
-  }
-});
+  });
+};
